a821ee424212af7ac5b81c851fdab99def00bb22
PrisM <thibault.fouache@gmail.com>
2019-04-16 21:18:36 +0200

    Suspendable contract with required methods and events.

--- contracts/Suspendable.sol
+++ contracts/Suspendable.sol
@@ -5,11 +5,30 @@ import "./Ownable.sol";
 contract Suspendable is Ownable {
     bool isRunning;
 
-    event LogSuspendable(address who);
+    event LogSuspend(address who);
+    event LogWakeUp(address who);
+
+    modifier ifRunning() {
+        require(isRunning);
+        _;
+    }
+
+    modifier ifSuspended() {
+        require(!isRunning);
+        _;
+    }
 
     constructor() public {
         isRunning = true;
     }
 
+    function suspend() public isOwner ifRunning {
+        isRunning = false;
+        emit LogSuspend(msg.sender);
+    }
 
+    function wakeUp() public isOwner ifSuspended {
+        isRunning = true;
+        emit LogWakeUp(msg.sender);
+    }
 }
