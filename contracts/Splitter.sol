82b9e1e83aaa5ae6ecb90620def6917f33d9e052
PrisM <thibault.fouache@gmail.com>
2019-04-16 22:35:53 +0200

    First iteration on Splitter test have to be done but to check if I am going in the right direction.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,5 +3,41 @@ pragma solidity 0.5.7;
 import "./Suspendable.sol";
 
 contract Splitter is Suspendable {
+    address private firstFriend;
+    address private secondFriend;
 
+    event LogFriendAdded(address who);
+    event LogSplit(address first, address second, uint howmuch);
+
+    modifier areFriendsThere() {
+        require(firstFriend != address(0x0) && secondFriend != address(0x0));
+        _;
+    }
+
+    function setFirstFriend(address who) public isOwner ifRunning {
+        firstFriend = who;
+        emit LogFriendAdded(who);
+    }
+
+    function setSecondFriend(address who) public isOwner ifRunning {
+        secondFriend = who;
+        emit LogFriendAdded(who);
+    }
+
+    function viewFriends() view public returns (address first, address second) {
+        first = firstFriend;
+        second = secondFriend;
+    }
+
+    function getBalance() public view returns (uint balance) {
+        balance = address(this).balance;
+    }
+
+    function split(uint amount) public payable areFriendsThere isOwner ifRunning returns (bool status) {
+        uint splitAmount = getBalance() / 2;
+        firstFriend.transfer(splitAmount);
+        secondFriend.transfer(splitAmount);
+        emit LogSplit(firstFriend, secondFriend, splitAmount);
+        status = true;
+    }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -36,27 +36,13 @@ module.exports = {
      */
 
     networks: {
-        // Useful for testing. The `development` name is special - truffle uses it by default
-        // if it's defined here and no other network is specified at the command line.
-        // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-        // tab if you use this network and you must also set the `host`, `port` and `network_id`
-        // options below to some value.
-        //
-        // development: {
-        //  host: "127.0.0.1",     // Localhost (default: none)
-        //  port: 8545,            // Standard Ethereum port (default: none)
-        //  network_id: "*",       // Any network (default: none)
-        // },
-
-        // Another network with more advanced options...
-        // advanced: {
-        // port: 8777,             // Custom port
-        // network_id: 1342,       // Custom network
-        // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-        // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-        // from: <address>,        // Account to send txs from (default: accounts[0])
-        // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-        // },
+        42: {
+            port: 30303,
+            network_id: 42,
+            gas: 8500000,
+            gasPrice: 20000000000,
+            websockets: true
+        },
 
         // Useful for deploying to a public network.
         // NB: It's important to wrap the provider as a function.
@@ -78,22 +64,16 @@ module.exports = {
     },
 
     // Set default mocha options here, use special reporters etc.
-    mocha: {
-        // timeout: 100000
-    },
+    mocha:
+        {
+            // timeout: 100000
+        }
+    ,
 
-    // Configure your compilers
+// Configure your compilers
     compilers: {
         solc: {
-            version: "0.5.7"        // Fetch exact version from solc-bin (default: truffle's version)
-            // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-            // settings: {          // See the solidity docs for advice about optimization and evmVersion
-            //  optimizer: {
-            //    enabled: false,
-            //    runs: 200
-            //  },
-            //  evmVersion: "byzantium"
-            // }
+            version: "0.5.7"
         }
     }
 };
