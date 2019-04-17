0161c51748fc9e062fe706047e16f2317b5484d0
PrisM <thibault.fouache@gmail.com>
2019-04-17 23:30:23 +0200

    Thanks Rob for comments. Hope this is getting better.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,41 +3,52 @@ pragma solidity 0.5.7;
 import "./Suspendable.sol";
 
 contract Splitter is Suspendable {
-    address payable private firstFriend;
-    address payable private secondFriend;
+    // Our two friends here with a mapping to keep track of their wealth.
+    address private firstFriend;
+    address private secondFriend;
+    mapping(address => uint) balances;
 
-    event LogFriendAdded(address who);
-    event LogSplit(address first, address second, uint howmuch);
+    // To deal with division remainder.
+    uint private bonus;
 
-    modifier areFriendsThere() {
-        require(firstFriend != address(0x0) && secondFriend != address(0x0));
-        _;
+    event LogLoad(address initiator, uint howMuch, uint bonus);
+    event LogWithdraw(address who, uint howMuch);
+
+    // Friends are set directly in the constructor and cannot  be changed afterwards. Valid addresses have to be provided for both of them.
+    constructor(address _firstFriend, address _secondFriend) public {
+        require(_firstFriend != address(0x0) && _secondFriend != address(0x0), "Please provide valid addresses");
+        firstFriend = _firstFriend;
+        secondFriend = _secondFriend;
     }
 
-    function setFirstFriend(address payable who) public isOwner ifRunning {
-        firstFriend = who;
-        emit LogFriendAdded(who);
+    // Small helper here. Mostly to experiment pure functions.
+    function divide(uint amount) private pure returns (uint div, uint rem) {
+        div = amount / 2;
+        rem = amount % 2;
     }
 
-    function setSecondFriend(address payable who) public isOwner ifRunning {
-        secondFriend = who;
-        emit LogFriendAdded(who);
+    // Loading the contract.
+    function load() public payable isOwner ifRunning {
+        require(msg.value > 0, "Please load contract with sufficient funds");
+        (uint divided, uint remainder) = divide(msg.value);
+        bonus += remainder;
+        balances[firstFriend] += divided;
+        balances[secondFriend] += divided;
+
+        // If bonus can be divided by 2 without remainder we can then add this bonus to each friend's wealth.
+        if (bonus % 2 == 0) {
+            balances[firstFriend] += 1;
+            balances[secondFriend] += 1;
+        }
+
+        emit LogLoad(msg.sender, msg.value, bonus);
     }
 
-    function viewFriends() view public returns (address first, address second) {
-        first = firstFriend;
-        second = secondFriend;
-    }
-
-    function getBalance() public view returns (uint balance) {
-        balance = address(this).balance;
-    }
-
-    function split() public payable areFriendsThere isOwner ifRunning returns (bool status) {
-        uint splitAmount = getBalance() / 2;
-        firstFriend.transfer(splitAmount);
-        secondFriend.transfer(splitAmount);
-        emit LogSplit(firstFriend, secondFriend, splitAmount);
-        status = true;
+    // Withdraw pattern (thanks Rob for the help).
+    function withdraw() public ifRunning {
+        uint available = balances[msg.sender];
+        require(available > 0, "Nothing to withdraw here");
+        address(msg.sender).transfer(available);
+        emit LogWithdraw(msg.sender, available);
     }
 }
