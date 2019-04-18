b8f56fe1cc1a91c878008dd1ed2c001789526ac6
PrisM <thibault.fouache@gmail.com>
2019-04-18 14:36:43 +0200

    Splitter is now not limited to fixed declared accounts.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,23 +3,15 @@ pragma solidity 0.5.7;
 import "./Suspendable.sol";
 
 contract Splitter is Suspendable {
-    // Our two friends here with a mapping to keep track of their wealth.
-    address private firstFriend;
-    address private secondFriend;
+    // Map of address and related wealth.
     mapping(address => uint) balances;
 
-    // To deal with division remainder.
+    // To deal with division remainder, contract owner will keep it for him as a gift from users :).
     uint private bonus;
 
     event LogLoad(address initiator, uint howMuch, uint bonus);
     event LogWithdraw(address who, uint howMuch);
-
-    // Friends are set directly in the constructor and cannot  be changed afterwards. Valid addresses have to be provided for both of them.
-    constructor(address _firstFriend, address _secondFriend) public {
-        require(_firstFriend != address(0x0) && _secondFriend != address(0x0), "Please provide valid addresses");
-        firstFriend = _firstFriend;
-        secondFriend = _secondFriend;
-    }
+    event LogWithdrawBonus(address who, uint howMuch);
 
     // Small helper here. Mostly to experiment pure functions.
     function divide(uint amount) private pure returns (uint div, uint rem) {
@@ -28,20 +20,13 @@ contract Splitter is Suspendable {
     }
 
     // Loading the contract.
-    function load() public payable isOwner ifRunning {
+    function splitFunds(address firstRecipient, address secondRecipient) public payable ifRunning {
         require(msg.value > 0, "Please load contract with sufficient funds");
+        require(firstRecipient != address(0x0) && secondRecipient != address(0x0), "Please provide valid addresses");
         (uint divided, uint remainder) = divide(msg.value);
         bonus += remainder;
-        balances[firstFriend] += divided;
-        balances[secondFriend] += divided;
-
-        // If bonus can be divided by 2 without remainder we can then add this bonus to each friend's wealth.
-        if (bonus % 2 == 0) {
-            uint split_bonus = bonus / 2;
-            balances[firstFriend] += split_bonus;
-            balances[secondFriend] += split_bonus;
-        }
-
+        balances[firstRecipient] += divided;
+        balances[secondRecipient] += divided;
         emit LogLoad(msg.sender, msg.value, bonus);
     }
 
@@ -51,5 +36,11 @@ contract Splitter is Suspendable {
         require(available > 0, "Nothing to withdraw here");
         address(msg.sender).transfer(available);
         emit LogWithdraw(msg.sender, available);
+    }
+
+    function withdrawBonus() isOwner ifRunning public {
+        require(bonus > 0, "Nothing to withdraw here");
+        address(msg.sender).transfer(bonus);
+        emit LogWithdrawBonus(owner, bonus);
     }
 }
