1df18760af1b31d6e962d97fb2c2917617890d8f
PrisM <thibault.fouache@gmail.com>
2019-04-22 00:14:57 +0200

    Fix the linkage of lib and contract.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,6 @@ const Splitter = artifacts.require("Splitter");
 
 module.exports = async function (deployer) {
     await deployer.deploy(Math);
-    await deployer.deploy(Splitter);
     await deployer.link(Math, Splitter);
+    await deployer.deploy(Splitter);
 };
