7c3f57a5834265e8c78477cf14d638ef3773cd63
PrisM <thibault.fouache@gmail.com>
2019-04-22 00:45:21 +0200

    Ownable tests.

--- test/SplitterTest.js
+++ test/SplitterTest.js
@@ -1,9 +1,27 @@
 const Splitter = artifacts.require("./Splitter.sol");
 
 contract("Splitter", async accounts => {
-    it("Should put the caller as owner", async () => {
-        let instance = await Splitter.deployed();
-        console.log(accounts);
-        assert.equal(10, 20 / 2);
+    let alice;
+    let bob;
+    let carol;
+    let splitter;
+
+    beforeEach('setup contract for each test', async function () {
+        alice = accounts[0];
+        bob = accounts[1];
+        carol = accounts[2];
+        splitter = await Splitter.new({from: alice});
+    });
+
+    it("Should allow the owner to transfer ownership", async () => {
+        assert.isOk(await splitter.changeOwnership(carol, {from: alice}));
+    });
+
+    it("Should disallow a random user to obtain ownership", async () => {
+        try {
+            await splitter.changeOwnership(bob, {from: bob});
+        } catch (e) {
+            assert.notEqual(e.message.indexOf("revert"), -1, 'This should be a revert error');
+        }
     });
 });
