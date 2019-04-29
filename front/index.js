65f29dd0ea21178c3052e1589ad36d4b81cbd1d0
PrisM <thibault.fouache@gmail.com>
2019-04-29 23:01:01 +0200

    Start frontend dev.

---
+++ front/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import Home from 'home'
+
+new Vue(Home).$mount('#app');
---
+++ front/src/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="myDiv">Hello {{appName}}</div>
+</template>
+
+<script>
+    export default {
+        name: "home",
+        data() {
+            return {
+                appName: "Splitter"
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .myDiv
+        color: blue
+</style>
